f22685155b8e511b50be701506b5f5205d3f48f9
spencerj21 <spencerj21@chcstudent.com>
2020-09-26 19:21:26 -0400

    create new pros project

---
+++ code/src/main.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void initialize() {}
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {}
+
+void opcontrol() {
+
+	while(true){
+		pros::delay(10);
+	}
+}
