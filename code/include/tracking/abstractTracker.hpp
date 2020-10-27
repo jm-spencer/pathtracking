4c32c4bd30a258687dd91922e2fea157392ab1b2
SpencerJ <spencerj21@chcstudent.com>
2020-10-27 11:53:56 -0400

    allow any kappa input to provide points

--- code/include/tracking/abstractTracker.hpp
+++ code/include/tracking/abstractTracker.hpp
@@ -2,9 +2,9 @@
 #include "kappa/api.hpp"
 
 template <std::size_t N>
-class AbstractTracker : public kappa::AbstractController<std::array<double,6>,std::shared_ptr<kappa::FileInput<N>>,std::tuple<double,double>> {
+class AbstractTracker : public kappa::AbstractController<std::array<double,6>,std::shared_ptr<kappa::AbstractInput<std::array<double,N>>>,std::tuple<double,double>> {
 public:
-  virtual void setTarget(const std::shared_ptr<kappa::FileInput<N>> &itarget) override {
+  virtual void setTarget(const std::shared_ptr<kappa::AbstractInput<std::array<double,N>>> &itarget) override {
     if(pathFile) {
       delete pathFile;
     }
@@ -16,6 +16,6 @@ public:
   }
 
 protected:
-  std::shared_ptr<kappa::FileInput<N>> pathFile{nullptr};
+  std::shared_ptr<kappa::AbstractInput<std::array<double,N>>> pathFile{nullptr};
   std::array<double,N> activeWaypoint;
 };
