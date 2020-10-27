88693035efcca1d1065d7106db77869747ac35f2
SpencerJ <spencerj21@chcstudent.com>
2020-10-27 13:24:11 -0400

    fix waypoint iterator in goal point selector

--- code/src/tracking/lookaheadTracker.cpp
+++ code/src/tracking/lookaheadTracker.cpp
@@ -1,7 +1,10 @@
 #include "tracking/lookaheadTracker.hpp"
 
 template<> std::array<double,2> LookaheadTracker<2>::getGoalPoint(double robotx, double roboty, double effectiveLookaheadSqr){
-  if(activeWaypoint[0] * activeWaypoint[0] + activeWaypoint[1] * activeWaypoint[1] < effectiveLookaheadSqr){
+  double deltaRX = activeWaypoint[0] - robotx;
+  double deltaRY = activeWaypoint[1] - roboty;
+
+  if(deltaRX * deltaRX + deltaRY * deltaRY < effectiveLookaheadSqr){
     std::copy(activeWaypoint.begin(), activeWaypoint.end(), lastWaypoint.begin());
     activeWaypoint = pathFile->get();
     return getGoalPoint(robotx, roboty, effectiveLookaheadSqr);
@@ -9,8 +12,6 @@ template<> std::array<double,2> LookaheadTracker<2>::getGoalPoint(double robotx,
 
   double deltaPX = activeWaypoint[0] - lastWaypoint[0];
   double deltaPY = activeWaypoint[1] - lastWaypoint[1];
-  double deltaRX = activeWaypoint[0] - robotx;
-  double deltaRY = activeWaypoint[1] - roboty;
 
   double a = deltaPX * deltaPX + deltaPY * deltaPY;
   double b = deltaPX * deltaRX + deltaPY * deltaRY;
@@ -23,7 +24,10 @@ template<> std::array<double,2> LookaheadTracker<2>::getGoalPoint(double robotx,
 }
 
 template<> std::array<double,3> LookaheadTracker<3>::getGoalPoint(double robotx, double roboty, double effectiveLookaheadSqr){
-  if(activeWaypoint[0] * activeWaypoint[0] + activeWaypoint[1] * activeWaypoint[1] < effectiveLookaheadSqr){
+  double deltaRX = activeWaypoint[0] - robotx;
+  double deltaRY = activeWaypoint[1] - roboty;
+
+  if(deltaRX * deltaRX + deltaRY * deltaRY < effectiveLookaheadSqr){
     std::copy(activeWaypoint.begin(), activeWaypoint.end(), lastWaypoint.begin());
     activeWaypoint = pathFile->get();
     return getGoalPoint(robotx, roboty, effectiveLookaheadSqr);
@@ -31,8 +35,6 @@ template<> std::array<double,3> LookaheadTracker<3>::getGoalPoint(double robotx,
 
   double deltaPX = activeWaypoint[0] - lastWaypoint[0];
   double deltaPY = activeWaypoint[1] - lastWaypoint[1];
-  double deltaRX = activeWaypoint[0] - robotx;
-  double deltaRY = activeWaypoint[1] - roboty;
 
   double a = deltaPX * deltaPX + deltaPY * deltaPY;
   double b = deltaPX * deltaRX + deltaPY * deltaRY;
