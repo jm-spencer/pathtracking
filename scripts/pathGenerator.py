b4447c9f2267b74c0f201e542f682ab313e014ec
spencerj21 <spencerj21@chcstudent.com>
2020-11-08 00:45:40 -0500

    simplify how path parameters are stored in the pathGenerator

--- scripts/pathGenerator.py
+++ scripts/pathGenerator.py
@@ -8,25 +8,26 @@ tStep = 0.0001
 
 # i_1 = curve#; i_2 = var#; i_3 = param#
 parameters = [
-  [[0,10,10,10],[0,0,10,0]],
-  [[10,10,20,0],[10,0,0,-10]]
+  [[0,10],[0,0]],
+  [[10,10],[10,0]],
+  [[20,0],[0,-10]]
 ]
 
 # given a hermite curve h defined by params, return h(t)
 
-def hermite(params, t):
-  return (2 * (t ** 3) - 3 * (t ** 2) + 1) * params[0] + \
-         ((t ** 3) - 2 * (t ** 2) + t) * params[1] + \
-         (-2 * (t ** 3) + 3 * (t ** 2)) * params[2] + \
-         ((t ** 3) - (t ** 2)) * params[3]
+def hermite(params0, params1, t):
+  return (2 * (t ** 3) - 3 * (t ** 2) + 1) * params0[0] + \
+         ((t ** 3) - 2 * (t ** 2) + t) * params0[1] + \
+         (-2 * (t ** 3) + 3 * (t ** 2)) * params1[0] + \
+         ((t ** 3) - (t ** 2)) * params1[1]
 
 # given a hermite curve h defined by params, return dh/dt (t)
 
-def dHermite(params, t):
-  return (6 * (t ** 2) - 6 * t) * params[0] + \
-         (3 * (t ** 2) - 4 * t + 1) * params[1] + \
-         (-6 * (t ** 2) + 6 * t) * params[2] + \
-         (3 * (t ** 2) - 2 * t) * params[3]
+def dHermite(params0, params1, t):
+  return (6 * (t ** 2) - 6 * t) * params0[0] + \
+         (3 * (t ** 2) - 4 * t + 1) * params0[1] + \
+         (-6 * (t ** 2) + 6 * t) * params1[0] + \
+         (3 * (t ** 2) - 2 * t) * params1[1]
 
 # path fn can be modified by user to generate CSV and binary path file for any functionally defined path
 # this implimentation uses a parametric cubic hermite spline
@@ -37,17 +38,17 @@ def path(t):
   returns path point
   """
   if(t != 1):
-    t *= len(parameters)
+    t *= (len(parameters) - 1)
     i = math.floor(t)
     t_i = t % 1
   else:
-    i = len(parameters) - 1
+    i = len(parameters) - 2
     t_i = 1
 
-  return array('d',[hermite(parameters[i][0],t_i),
-                    hermite(parameters[i][1],t_i),
-                    math.atan2(dHermite(parameters[i][1],t_i),
-                               dHermite(parameters[i][0],t_i))])
+  return array('d',[hermite(parameters[i][0], parameters[i+1][0], t_i),
+                    hermite(parameters[i][1], parameters[i+1][1], t_i),
+                    math.atan2(dHermite(parameters[i][1], parameters[i+1][1], t_i),
+                               dHermite(parameters[i][0], parameters[i+1][0],t_i))])
 
 csvfile = open("path.csv", mode='w', encoding='utf-8')
 binfile = open("path", mode='wb')
