05eaf4af32503928e37282eb24280be0e1f6c7f3
spencerj21 <spencerj21@chcstudent.com>
2020-11-10 19:54:22 -0500

    add csv reading to imageGenerator

--- scripts/imageGenerator.py
+++ scripts/imageGenerator.py
@@ -2,6 +2,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 parameters = [
   [[0  , 100], [0  ,  0  ]],
@@ -36,7 +37,23 @@ for i in range(len(parameters) - 1):
 
   ax.plot(sx, sy, 'b')
 
+points = csv.reader(open('paths/path2.csv'))
+
+#for point in points:
+#    ax.plot(float(point[0]), float(point[1]),'b')
+
+columns = [[],[],[],[]]
+for point in points:
+    for i in range(4):
+        try:
+            columns[i].append(float(point[i]))
+        except:
+            pass
+
+ax.plot(columns[0], columns[1], 'r')
+
 ax.set(xlabel='X (cm)', ylabel='Y (cm)', title='Path')
 ax.grid()
 
 fig.savefig("pathImage.png")
+plt.show()
