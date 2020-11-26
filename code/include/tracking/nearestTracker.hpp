8a2eb70eec838fd0b520fdc79208e2a3b992a685
spencerj21 <spencerj21@chcstudent.com>
2020-11-25 21:04:13 -0500

    start work on nearestTracker, for algorithms that track nearest point

---
+++ code/include/tracking/nearestTracker.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "kappa/api.hpp"
+#include "tracking/abstractTracker.hpp"
+
+template <std::size_t N>
+class NearestTracker: public AbstractTracker<N> {
+public:
+  NearestTracker(){
+    lastWaypoint.fill(0);
+  }
+
+  std::array<double,N> getGoalPoint(double robotx, double roboty);
+  
+protected:
+  double waypointIndex{0};
+
+  std::array<double,N> lastWaypoint;
+};
